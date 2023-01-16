92ee5a219b8fb494a7c95d5dfe8dcb4d7696e864
jpfbarreira <jpfbarreira@gmail.com>
2023-01-16 18:37:50 +0000

    dancing activity

--- app/src/main/java/com/example/beguest/DancingActivity.java
+++ app/src/main/java/com/example/beguest/DancingActivity.java
@@ -21,20 +21,20 @@ public class DancingActivity extends AppCompatActivity {
 
 
         //sure you want to exit - not working
-//        backBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                new AlertDialog.Builder(getApplicationContext())
-//                        .setTitle("Really Exit?")
-//                        .setMessage("Are you sure you want to exit?")
-//                        .setNegativeButton("no", null)
-//                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                onBackPressed();
-//                            }
-//                        });
-//            }
-//        });
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("Really Exit?")
+                        .setMessage("Are you sure you want to exit?")
+                        .setNegativeButton("no", null)
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                onBackPressed();
+                            }
+                        });
+            }
+        });
     }
 }
